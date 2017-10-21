84a9975d02d60a87dc85bacd43af9fb4517240c2
Min Kim <mk4555@nyu.edu>
2017-10-21 16:02:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(string)
   if string.upcase == string
     if string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    return 'I LOVE YOU TOO PUMPKIN'
+    else
+      return 'NO, NOT SINCE 1938!'
   else
     return 'HUH? SPEAK UP, SONNY!'
   end
