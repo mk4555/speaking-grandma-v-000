31fa82949bbac0a9ab8a68d369b3b7b3a539a3a2
Min Kim <mk4555@nyu.edu>
2017-10-21 16:02:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if string.upcase == string
+    if string == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
     return 'I LOVE YOU TOO PUMPKIN'
   else
     return 'HUH? SPEAK UP, SONNY!'
