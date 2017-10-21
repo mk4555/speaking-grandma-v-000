45ce35f2baf75217b174e6ccc190777f937ac6c8
Min Kim <mk4555@nyu.edu>
2017-10-21 15:58:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,8 @@ def speak_to_grandma(string)
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
